14d05a6449c0a5fba1b8bc2e5c6c2a15e1b7f816
VincentCCandela <vincent.candela14@gmail.com>
2019-05-09 09:53:59 -0400

    asdfasdf

--- game_of_life.cpp
+++ game_of_life.cpp
@@ -3,9 +3,37 @@
 
 using namespace std;
 
-int main(){
-    int row = 20, column = 20,grid[row][column], temp_grid;
+int main()
+{
+    const int row = 20, column = 20
+    int x_coordinate, y_coordinate;
+    char response, grid[row][column], temp_grid[row][column];
 
+    for(int i = 0; i < row; ++i)
+    {
+        for(int j = 0; j < col; ++j)
+        {
+            grid[i][j] = 'O';
+            temp_grid[i][j] = grid[i][j];
+        }
+    }
+
+    cout << "Do you want to enter a live cell into the grid? (y/n) ";
+    cin >> response;
+
+    while (toupper(response) == 'Y')
+    {
+        cout << "Enter the x coordinate of the cell: "
+        cin >> x_coordinate;
+
+        cout << "Enter the y coordinate of the cell: "
+        cin >> y_coordiante;
+
+        grid[x_coordinate][  20 - y_coordinate ] = 'X'
+
+        cout << "Do you want to enter another live cell into the grid? (y/n) ";
+        cin >> response;
+    }
 
     return 0;
 }

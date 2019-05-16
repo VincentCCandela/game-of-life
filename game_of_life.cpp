cef46425cc609ec43c1f3e8e66a1c561d4d546c1
VincentCCandela <vincent.candela14@gmail.com>
2019-05-16 11:56:14 -0400

    asdfasdfasdf

--- game_of_life.cpp
+++ game_of_life.cpp
@@ -4,13 +4,13 @@
 using namespace std;
 
 const int row = 20, column = 20; //size of grid
-int full_checker(int r, int c, int orginial[row][column]); //creates function to check cell is if alive or dead (true or false)
-void changer(int r, int c, int original[row][column]); //creates function to change grid values
+int checker(int r, int c, char orginial[row][column]); //creates function to check cell is if alive or dead (true or false)
+void changer(int r, int c, char grid[][column], char temp_grid[][column]);
 
 int main()
 {
     int x_coordinate, y_coordinate; //input for desired coordinates of cells
-    char response, reponse2, grid[row][column], temp_grid[row][column]; //creates grid, response1, and reponse2
+    char response, response2, grid[row][column], temp_grid[row][column]; //creates grid, response1, and reponse2
 
     for(int i = 0; i < row; ++i)
     {
@@ -29,21 +29,12 @@ int main()
     }
 
     cout << "Do you want to enter a live cell into the grid? (y/n) ";
-    cin >> response2;
+    cin >> response;
 
-    while(toupper(response2) == 'Y')
-    {
-        for(int i = 0; i < row; ++i)
-        {
-            for(int j = 0; j < column; ++j)
-            {
-                changer(r,c,original[r][c]); //calls changer function for grid
-            }
-        }
-
+    do{
         while (toupper(response) == 'Y') //when response is yes, will let you selct where you want new cell
         {
-           cout << "Enter the x coordinate of the cell: ";
+            cout << "Enter the x coordinate of the cell: ";
             cin >> x_coordinate;
 
             cout << "Enter the y coordinate of the cell: ";
@@ -56,6 +47,7 @@ int main()
                 for(int j = 0; j < column; ++j)
                 {
                     cout << setw(4) << grid[i][j]; //outputs updated grid
+                    temp_grid[i][j] = grid[i][j];
                 }
             }
 
@@ -63,18 +55,34 @@ int main()
             cin >> response;
         }
 
-        cout << "Do you want to go to the next day?";
-        cin >> response2;
-    }
+        for(int i = 0; i < row; ++i)
+        {
+            for(int j = 0; j < column; ++j)
+            {
+                changer(i,j,grid,temp_grid); //calls changer function for grid
+            }
+        }
 
-    cout << endl;
+        for(int i = 0; i < row; ++i)
+        {
+            for(int j = 0; j < column; ++j)
+            {
+                grid[i][j] = temp_grid[i][j];            
+                cout << setw(4) << grid[i][j]; //outputs updated grid
+            }
+        }
+
+        cout << "Do you want to go to the next day? (y/n)";
+        cin >> response2;
+        
+    } while(toupper(response2) == 'Y');
 
     cout << "Thanks for playing!" << endl;
 
     return 0;
 }
 
-int full_checker(int r, int c, int original[][column]){
+int checker(int r, int c, char original[][column]){
     int counter;
     if(r == 0 && c == 0){
         if(original[r][c + 1] == 'X'){
@@ -220,7 +228,7 @@ int full_checker(int r, int c, int original[][column]){
         else if(counter == 2) {
             return 2;
         }
-        else if (couter > 3){
+        else if (counter > 3){
             return 4;
         }
         else{
@@ -229,14 +237,16 @@ int full_checker(int r, int c, int original[][column]){
     }
 }
 
-void changer(int r, int c, char grid[][column], char temp_array[][column])
+void changer(int r, int c, char grid[][column], char temp_grid[][column])
 {
-    if(grid[r][c] == 'O' && ( checker(r,c,grid) == 3 || checker(r,c,grid) == 2 )
+    if(grid[r][c] == 'O' && ( checker(r,c,grid) == 3 || checker(r,c,grid) ) == 2 )
     {
-        temp_array[r][c] = 'X';
+        temp_grid[r][c] = 'X';
     }
     else if(grid[r][c] == 'X' && (checker(r,c,grid) == 4 || checker(r,c,grid) == 1) ) //sets all false values to showing no cell(dead)
     {
-        temp_array[r][c] = 'O';
+        temp_grid[r][c] = 'O';
     }
-}
+} //hi vincent
+
+//cout << "I am GAr";
